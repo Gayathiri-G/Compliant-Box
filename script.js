e78c9cd6da91316ad3eda9597dc8e9652ffa3e01
Gayathiri G <gayathiri848962@gmail.com>
2026-04-28 17:29:46 +0530

    updated code logic abd styling

--- script.js
+++ script.js
@@ -1,94 +1,10 @@
-const list = document.getElementById("list");
+function showPage(page) {
+    document.getElementById("homePage").style.display = "none";
+    document.getElementById("aboutPage").style.display = "none";
 
-// Load data
-window.onload = function () {
-    displayComplaints(getComplaints());
-
-    if (localStorage.getItem("theme") === "dark") {
-        document.body.classList.add("dark");
-    }
-};
-
-// Add complaint
-function addComplaint() {
-    const title = document.getElementById("title").value.trim();
-    const desc = document.getElementById("desc").value.trim();
-
-    if (!title || !desc) return;
-
-    const complaint = {
-        id: Date.now(),
-        title,
-        desc,
-        date: new Date().toLocaleString()
-    };
-
-    let data = getComplaints();
-    data.unshift(complaint);
-    saveComplaints(data);
-
-    displayComplaints(data);
-
-    document.getElementById("title").value = "";
-    document.getElementById("desc").value = "";
-}
-
-// Display
-function displayComplaints(data) {
-    list.innerHTML = "";
-
-    data.forEach(item => {
-        const div = document.createElement("div");
-        div.className = "card";
-
-        div.innerHTML = `
-            <h3>${item.title}</h3>
-            <p>${item.desc}</p>
-            <small>${item.date}</small><br>
-            <button onclick="deleteComplaint(${item.id})">Delete</button>
-        `;
-
-        list.appendChild(div);
-    });
-}
-
-// Delete
-function deleteComplaint(id) {
-    let data = getComplaints().filter(c => c.id !== id);
-    saveComplaints(data);
-    displayComplaints(data);
-}
-
-// Search
-function searchComplaint() {
-    const value = document.getElementById("search").value.toLowerCase();
-    const data = getComplaints();
-
-    const filtered = data.filter(c =>
-        c.title.toLowerCase().includes(value) ||
-        c.desc.toLowerCase().includes(value)
-    );
-
-    displayComplaints(filtered);
-}
-
-// Logout
-function logout() {
-    localStorage.removeItem("login");
-    window.location.href = "login.html";
-}
-
-// Theme
-function toggleTheme() {
-    document.body.classList.toggle("dark");
-
-    if (document.body.classList.contains("dark")) {
-        localStorage.setItem("theme", "dark");
+    if (page === "home") {
+        document.getElementById("homePage").style.display = "block";
     } else {
-        localStorage.setItem("theme", "light");
+        document.getElementById("aboutPage").style.display = "block";
     }
-<<<<<<< HEAD
 }
-=======
-}
->>>>>>> c6dcab0fccaaea6838947b51b85a7ff5c2d9e15d
